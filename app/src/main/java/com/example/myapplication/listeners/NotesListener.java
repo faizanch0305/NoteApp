38180e5bc64e2cf5cbebcd584f8c77451465d18c
Muhammad Faizan Iqbal <faizanch0305@users.noreply.github.com>
2021-08-22 12:07:36 +0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/listeners/NotesListener.java
@@ -0,0 +1,9 @@
+package com.example.myapplication.listeners;
+
+import com.example.myapplication.entities.Note;
+
+public interface NotesListener {
+
+    void onNoteClicked(Note note, int position);
+
+}
